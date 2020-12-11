616a70695bb8365c53df931456b3ed0a36cd09f0
Timofey Bakunin <bakunin.t@protonmail.com>
2020-12-11 20:39:20 +0300

    loader fix

--- loaders.py
+++ loaders.py
@@ -2,9 +2,10 @@ import subprocess
 import json
 from collections import defaultdict, MutableSequence
 import pandas as pd
+import logging
 
 
-class HTTPPacket():
+class HTTPPacket:
     def __init__(self, http, tcp_seq, ip_src, ip_dst):
         self.sessionized = False
         new_http = defaultdict(str)
@@ -16,23 +17,63 @@ class HTTPPacket():
         self.ip_src = ip_src
         self.ip_dst = ip_dst
         self.http = {}
-        if 'http.request' in http.keys():
-            self.http["type"] = 'REQ'
-            self.http["method"] = http["http.request.method" ]
-            self.http["uri"] = http["http.request.uri"]
-            self.http["version"] = http["http.request.version"]
+        if "http.request" in http.keys():
+            self.http["type"] = "REQ"
+            for k in http.keys():
+                if isinstance(http[k], dict):
+                    if "_ws.expert" in http[k].keys():
+                        self.http["method"] = http[k]["http.request.method"]
+                        if self.http["method"] == "":
+                            logging.warning('Blank at {self.http["method"]}')
+                        self.http["uri"] = http[k]["http.request.uri"]
+                        if self.http["uri"] == "":
+                            logging.warning('Blank at {self.http["uri"]}')
+                        self.http["version"] = http[k]["http.request.version"]
+                        if self.http["version"] == "":
+                            logging.warning('Blank at {self.http["version"]}')
+                        break
+
             self.http["host"] = http["http.host"]
+            if self.http["host"] == "":
+                logging.warning('Blank at {self.http["host"]}')
             self.http["ua"] = http["http.user_agent"]
+            if self.http["ua"] == "":
+                logging.warning('Blank at {self.http["ua"]}')
             self.http["accept_enc"] = http["http.accept_encoding"]
+            if self.http["accept_enc"] == "":
+                logging.warning('Blank at {self.http["accept_enc"]}')
             self.http["payload_length"] = http["http.content_length_header"]
+            if self.http["payload_length"] == "":
+                logging.debug('Blank at {self.http["payload_length"]}')
             self.http["data"] = http["http.file_data"]
-        if 'http.response' in http.keys():
-            self.http["type"] = 'RESP'
-            self.http["version"] = http["http.response.version"]
-            self.http["code"] = http["http.response.code"]
+            if self.http["data"] == "":
+                logging.debug('Blank at {self.http["data"]}')
+
+        elif "http.response" in http.keys():
+            self.http["type"] = "RESP"
+            for k in http.keys():
+                if isinstance(http[k], dict):
+                    if "_ws.expert" in http[k].keys():
+                        self.http["version"] = http[k]["http.response.version"]
+                        if self.http["version"] == "":
+                            logging.warning('Blank at {self.http["version"]}')
+                        self.http["code"] = http[k]["http.response.code"]
+                        if self.http["code"] == "":
+                            logging.warning('Blank at {self.http["code"]}')
+                        break
+
             self.http["header_length"] = http["http.content_length_header"]
+            if self.http["header_length"] == "":
+                logging.warning('Blank at {self.http["header_length"]}')
             self.http["server"] = http["http.server"]
+            if self.http["server"] == "":
+                logging.warning('Blank at {self.http["server"]}')
             self.http["data"] = http["http.file_data"]
+            if self.http["data"] == "":
+                logging.warning('Blank at {self.http["data"]}')
+        else:
+            raise ValueError("Could not determine packet type!")
+
 
     def __str__(self):
         return f"<HTTP TCPSQ:{self.tcp_seq} {self.http['type']}>"
@@ -65,10 +106,14 @@ class PacketList(MutableSequence):
             if isinstance(self._inner_list[0], list):
                 sessions = []
                 for session in self._inner_list:
-                    sessions.append('['+", ".join([str(p) for p in session])+']')
-                return 'PacketList['+", ".join([str(session) for session in sessions])+']'
+                    sessions.append("[" + ", ".join([str(p) for p in session]) + "]")
+                return (
+                    "PacketList["
+                    + ", ".join([str(session) for session in sessions])
+                    + "]"
+                )
         else:
-            return '[]'
+            return "[]"
 
     def sessionize(self):
         self.sessionized = True
@@ -81,7 +126,9 @@ class PacketList(MutableSequence):
 
     def to_pandas(self):
         if not self.sessionized:
-            raise ValueError("PacketList must be sessionized before conversion to pandas!")
+            raise ValueError(
+                "PacketList must be sessionized before conversion to pandas!"
+            )
         convlist = []
         for session in self._inner_list:
             tempdict = {}
@@ -99,17 +146,24 @@ class PacketList(MutableSequence):
         df = pd.DataFrame(convlist)
         return df
 
-class PcapLoader():
+
+class PcapLoader:
     """ Loader that uses pcap files """
+
     def __init__(self, filename):
-        pcap = subprocess.run(["tshark", "-r", filename, "-Y", "http", "-T", "json"], check=False, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout
+        pcap = subprocess.run(
+            ["tshark", "-r", filename, "-Y", "http", "-T", "json"],
+            check=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+        ).stdout
         pcap = json.loads(pcap.decode())
         self.packets = PacketList()
         for packet in pcap:
-            tcp_seq = int(packet['_source']['layers']['tcp']['tcp.stream'])
-            ip_dst = packet['_source']['layers']['ip']['ip.dst']
-            ip_src = packet['_source']['layers']['ip']['ip.src']
-            http = packet['_source']['layers']['http']
+            tcp_seq = int(packet["_source"]["layers"]["tcp"]["tcp.stream"])
+            ip_dst = packet["_source"]["layers"]["ip"]["ip.dst"]
+            ip_src = packet["_source"]["layers"]["ip"]["ip.src"]
+            http = packet["_source"]["layers"]["http"]
             new_packet = HTTPPacket(http, tcp_seq, ip_src, ip_dst)
             self.packets.append(new_packet)
 
